234f868cda2457a1c133960240fac5669b95fc06
Alexei Nicolaev <alniko009@gmail.com>
2013-11-27 15:28:32 +0600

    Updated

--- www/sites/all/themes/smartexe/js/jquery.timer.js
+++ www/sites/all/themes/smartexe/js/jquery.timer.js
@@ -1,72 +1,111 @@
 /**
- * jQuery Timer Plugin (jquery.timer.js)
- * @version 1.0.1
- * @author James Brooks <jbrooksuk@me.com>
- * @website http://james.brooks.so
- * @license MIT - http://jbrooksuk.mit-license.org
+ * jquery.timer.js
+ *
+ * Copyright (c) 2011 Jason Chavannes <jason.chavannes@gmail.com>
+ *
+ * http://jchavannes.com/jquery-timer
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
-(function($) {
-	jQuery.timer = function(interval, callback, options) {
-		// Create options for the default reset value
-		var options = jQuery.extend({ reset: 500 }, options);
-		var interval = interval || options.reset;
-
-		if(!callback) { return false; }
-
-		var Timer = function(interval, callback, disabled) {
-			// Only used by internal code to call the callback
-			this.internalCallback = function() { callback(self); };
-
-			// Clears any timers
-			this.stop = function() { 
-				if(this.state === 1 && this.id) {
-					clearInterval(self.id); 
-					this.state = 0;
-					return true;
-				}
-				return false;
-			};
-			// Resets timers to a new time
-			this.reset = function(time) {
-				if(self.id) { clearInterval(self.id); }
-				var time = time || options.reset;
-				this.id = setInterval($.proxy(this.internalCallback, this), time);
-				this.state = 1;
-				return true;
-			};
-			// Pause a timer.
-			this.pause = function() {
-				if(self.id && this.state === 1) {
-					clearInterval(this.id);
-					this.state = 2;
-					return true;
-				}
-				return false;
-			};
-			// Resumes a paused timer.
-			this.resume = function() {
-				if(this.state === 2) {
-					this.state = 1;
-					this.id = setInterval($.proxy(this.internalCallback, this), this.interval);
-					return true;
-				}
-				return false;
-			};
-
-			// Set the interval time again
-			this.interval = interval;
-			
-			// Set the timer, if enabled
-			if (!disabled) {
-				this.id = setInterval($.proxy(this.internalCallback, this), this.interval);
-				this.state = 1;
+;(function($) {
+	$.timer = function(func, time, autostart) {	
+	 	this.set = function(func, time, autostart) {
+	 		this.init = true;
+	 	 	if(typeof func == 'object') {
+		 	 	var paramList = ['autostart', 'time'];
+	 	 	 	for(var arg in paramList) {if(func[paramList[arg]] != undefined) {eval(paramList[arg] + " = func[paramList[arg]]");}};
+ 	 			func = func.action;
+	 	 	}
+	 	 	if(typeof func == 'function') {this.action = func;}
+		 	if(!isNaN(time)) {this.intervalTime = time;}
+		 	if(autostart && !this.isActive) {
+			 	this.isActive = true;
+			 	this.setTimer();
+		 	}
+		 	return this;
+	 	};
+	 	this.once = function(time) {
+			var timer = this;
+	 	 	if(isNaN(time)) {time = 0;}
+			window.setTimeout(function() {timer.action();}, time);
+	 		return this;
+	 	};
+		this.play = function(reset) {
+			if(!this.isActive) {
+				if(reset) {this.setTimer();}
+				else {this.setTimer(this.remaining);}
+				this.isActive = true;
 			}
-
-			var self = this;
+			return this;
 		};
-
-		// Create a new timer object
-		return new Timer(interval, callback, options.disabled);
+		this.pause = function() {
+			if(this.isActive) {
+				this.isActive = false;
+				this.remaining -= new Date() - this.last;
+				this.clearTimer();
+			}
+			return this;
+		};
+		this.stop = function() {
+			this.isActive = false;
+			this.remaining = this.intervalTime;
+			this.clearTimer();
+			return this;
+		};
+		this.toggle = function(reset) {
+			if(this.isActive) {this.pause();}
+			else if(reset) {this.play(true);}
+			else {this.play();}
+			return this;
+		};
+		this.reset = function() {
+			this.isActive = false;
+			this.play(true);
+			return this;
+		};
+		this.clearTimer = function() {
+			window.clearTimeout(this.timeoutObject);
+		};
+	 	this.setTimer = function(time) {
+			var timer = this;
+	 	 	if(typeof this.action != 'function') {return;}
+	 	 	if(isNaN(time)) {time = this.intervalTime;}
+		 	this.remaining = time;
+	 	 	this.last = new Date();
+			this.clearTimer();
+			this.timeoutObject = window.setTimeout(function() {timer.go();}, time);
+		};
+	 	this.go = function() {
+	 		if(this.isActive) {
+	 			this.action();
+	 			this.setTimer();
+	 		}
+	 	};
+	 	
+	 	if(this.init) {
+	 		return new $.timer(func, time, autostart);
+	 	} else {
+			this.set(func, time, autostart);
+	 		return this;
+	 	}
 	};
 })(jQuery);
