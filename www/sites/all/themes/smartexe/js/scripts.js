f0a4b54a1edb092e00f63b7d6ac8ba4cd37a92a2
anvar-sagutdinov <anvar@anvars-iMac.local>
2013-11-27 14:03:02 +0600

    theme and modules

---
+++ www/sites/all/themes/smartexe/js/scripts.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+	//head scripts
+	//career
+	$(".head .head_top_body_left_careers").on("mouseover", function(e) {
+		$(this).children().eq(0).children().eq(0).attr("src", "images/head/careers_hover.png");
+		$(this).children().eq(1).css("color", "#EF532C");
+	});
+	$(".head .head_top_body_left_careers").on("mouseout", function(e) {
+		$(this).children().eq(0).children().eq(0).attr("src", "images/head/careers_normal.png");
+		$(this).children().eq(1).css("color", "#ABABB1");
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//footer scripts
+	//sitemap
+	$(".footer .footer_top_body_right_content_item").on("mouseover", function(e) {
+		$(this).find("img").attr("src", "images/footer/sitemap_hover.png");
+	});
+	$(".footer .footer_top_body_right_content_item").on("mouseout", function(e) {
+		$(this).find("img").attr("src", "images/footer/sitemap_normal.png");
+	});
+	
+	
+	
+	//created
+	$(".footer .footer_bottom_body_right_created").on("mouseover", function(e) {
+		$(this).find("img").attr("src", "images/footer/insegment_logo_hover.png");
+	});
+	$(".footer .footer_bottom_body_right_created").on("mouseout", function(e) {
+		$(this).find("img").attr("src", "images/footer/insegment_logo_normal.png");
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ www/sites/all/themes/smartexe/template.php
@@ -0,0 +1,15 @@
+<?php
+function smartexe_js_alter(&$javascript) {
+    //We define the path of our new jquery core file
+
+    $jquery_path = drupal_get_path('theme','smartexe') . '/js/jquery-2.0.3.js';
+
+    //We duplicate the important information from the Drupal one
+    $javascript[$jquery_path] = $javascript['misc/jquery.js'];
+    //..and we update the information that we care about
+    $javascript[$jquery_path]['version'] = '2.0.3';
+    $javascript[$jquery_path]['data'] = $jquery_path;
+
+    //Then we remove the Drupal core version
+    unset($javascript['misc/jquery.js']);
+}
